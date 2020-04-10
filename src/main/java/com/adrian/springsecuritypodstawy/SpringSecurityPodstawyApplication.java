04610f17aeb26583fc07064205584b025211954d
Adiks <apacholak97@interia.pl>
2020-04-10 20:05:56 +0200

    Initial commit

---
+++ src/main/java/com/adrian/springsecuritypodstawy/SpringSecurityPodstawyApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.springsecuritypodstawy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPodstawyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPodstawyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrian/springsecuritypodstawy/SpringSecurityPodstawyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.springsecuritypodstawy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPodstawyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
